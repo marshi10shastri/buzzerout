4b5b09bb1539dbb8cf783a43cbbfcfc9308567db
Pratyush Aswal <30869332+pratyush-aswal@users.noreply.github.com>
2020-04-18 12:23:51 +0530

    added signup api

--- js/signUp.js
+++ js/signUp.js
@@ -1,27 +1,39 @@
-var signUpBtn = document.getElementById('signUpBtn');
-signUpBtn.addEventListener('click', signUp);
+// var signUpBtn = document.getElementById('signUpBtn');
+// signUpBtn.addEventListener('click', signUp());
 
+console.log("outside");
 function signUp(){
     console.log('hello');
-    var name = document.getElementById('exampleInputEmail1').value;
+    var name = document.getElementById('exampleInputEmail1').value.split(" ");
     var email = document.getElementById('exampleInputEmail2').value;
     var password = document.getElementById('exampleInputPassword1').value;
 
+    alert(name[0]);
+    console.log(name);
+    console.log(email);
+    console.log(password);
     $.ajax({
         type:'POST',
-        url:'',
+        url:'http://buzzerout.com/buzzerout_server/v1/user/register',
         data:{
-            name: name,
+            firstname: name[0],
+            lastname:name[1],
+            username:"asd",
             email: email,
             password: password
         },
 
         success: function(data){
+            // alert(data);
             console.log(data);
         },
 
         error: function(data){
+            alert("error");
             console.log(data);
         },
     });
+
+    console.log("function end");
 }
+console.log("end of file");
