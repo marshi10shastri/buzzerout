2ee59ce11ed8a9bf7c9ba08907f807a39af6ee7c
marshi <marshi10shastri@gmail.com>
2020-04-18 14:39:41 +0530

    Added username to api call

--- js/signUp.js
+++ js/signUp.js
@@ -2,33 +2,35 @@
 // signUpBtn.addEventListener('click', signUp());
 
 console.log("outside");
-function signUp(){
+
+function signUp() {
     console.log('hello');
     var name = document.getElementById('exampleInputEmail1').value.split(" ");
     var email = document.getElementById('exampleInputEmail2').value;
     var password = document.getElementById('exampleInputPassword1').value;
+    var usern = document.getElementById('exampleInputEmail0').value;
 
     alert(name[0]);
     console.log(name);
     console.log(email);
     console.log(password);
     $.ajax({
-        type:'POST',
-        url:'http://buzzerout.com/buzzerout_server/v1/user/register',
-        data:{
+        type: 'POST',
+        url: 'http://buzzerout.com/buzzerout_server/v1/user/register',
+        data: {
             firstname: name[0],
-            lastname:name[1],
-            username:"asd",
+            lastname: name[1],
+            username: usern,
             email: email,
             password: password
         },
 
-        success: function(data){
+        success: function(data) {
             // alert(data);
             console.log(data);
         },
 
-        error: function(data){
+        error: function(data) {
             alert("error");
             console.log(data);
         },
