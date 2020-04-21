cd7560c12513e4901afd0d85a84b35a52c77a0d1
Pratyush Aswal <30869332+pratyush-aswal@users.noreply.github.com>
2020-04-21 23:42:50 +0530

    profile about edit
    except editing already filled vaues of colleges, cities and work

--- js/profile.js
+++ js/profile.js
@@ -7,34 +7,132 @@ function showProfile(){
     var userDetails = document.getElementById('about').innerHTML;
     userDetails = '';
 
+    var currUser = getJSONLocalStorage(USER_INFO);
+
     // adding dummy values
-    userDetails += profile_template_contactInfo(DUMMY_USER.email, DUMMY_USER.mobile, DUMMY_USER.address)+
-                profile_template_websites(DUMMY_USER.website, DUMMY_USER.socialLink)+
-                profile_template_basicInfo(DUMMY_USER.dob, DUMMY_USER.yob, DUMMY_USER.gender, DUMMY_USER.interest, DUMMY_USER.language)+
+    userDetails += profile_template_contactInfo(currUser.email, currUser.mobile, currUser.address)+
+                profile_template_websites(currUser.website, currUser.socialLink)+
+                profile_template_basicInfo(currUser.dob, currUser.yob, currUser.gender, currUser.interest, currUser.language)+
                 profile_family()+
                 profile_template_work();
 
         // adding multiple workplaces
-        for(let i=0; i<DUMMY_USER.work.length; i++){
-            userDetails += profile_template_addWork(DUMMY_USER.work[i].workPlace, DUMMY_USER.work[i].workProfile);
+        for(let i=0; i<currUser.work.length; i++){
+            userDetails += profile_template_addWork(currUser.work[i].workPlace, currUser.work[i].workProfile);
         }
         
 
         userDetails += profile_template_professional_skill()+
-                    profile_template_college()+
-                    profile_template_addCollege(DUMMY_USER.college.collegeName, DUMMY_USER.college.collegePlace)+
-                    profile_template_city();
+                    profile_template_college();
+        
+        // adding college
+        for(let k=0; k<currUser.college.length; k++){
+                userDetails += profile_template_addCollege(currUser.college[k].collegeName, currUser.college[k].collegePlace);
+        }
+        
+        userDetails += profile_template_city();
         
         // adding multiple cities
-        for(let j=0; j<DUMMY_USER.city.length; j++){
-            userDetails += profile_template_addCity(DUMMY_USER.city[j].placeName, DUMMY_USER.city[j].placeState)
+        for(let j=0; j<currUser.city.length; j++){
+            userDetails += profile_template_addCity(currUser.city[j].placeName, currUser.city[j].placeState)
         }
         
 
         userDetails += profile_template_place_extra()+
-                    profile_template_about(DUMMY_USER.about, DUMMY_USER.otherName, DUMMY_USER.favQuote);
+                    profile_template_about(currUser.about, currUser.otherName, currUser.favQuote);
 
 
     // putting value back to the div
     document.getElementById('about').innerHTML = userDetails;
+}
+
+
+
+function editContactInfo(){
+    let user = getJSONLocalStorage(USER_INFO);
+    user.mobile = document.getElementById('mobileInput').value;
+    user.address = document.getElementById('addressInput').value;
+    setJSONLocalStorage(USER_INFO, user);
+    showProfile();
+}
+
+function editWebsite(){
+    let user = getJSONLocalStorage(USER_INFO);
+    user.website = document.getElementById('websiteInput').value;
+    user.socialLink = document.getElementById('socialInput').value;
+    setJSONLocalStorage(USER_INFO, user);
+    showProfile();
+}
+
+function editBasic(){
+    let user = getJSONLocalStorage(USER_INFO);
+    if(document.getElementById('maleRadio').checked){
+        user.gender = 'Male';
+    }
+    else{
+        user.gender = 'Female';
+    }
+
+    user.dob = document.getElementById('inputDob').value;
+    user.yob = document.getElementById('yearInput').value;
+    user.interest = document.getElementById('interestInput').value;
+    user.language = document.getElementById('languageInput').value;
+    setJSONLocalStorage(USER_INFO, user);
+    showProfile();
+}
+
+function addWork(){
+    let user = getJSONLocalStorage(USER_INFO);
+    let workIn={
+        workPlace: document.getElementById('workPlaceInput').value,
+        workProfile: document.getElementById('workProfileInput').value
+    };
+    user.work.push(workIn);
+    setJSONLocalStorage(USER_INFO, user);
+    showProfile();
+}
+
+function addCollege(){
+    let user = getJSONLocalStorage(USER_INFO);
+    let collegeIn={
+        collegeName: document.getElementById('collegeNameInput').value,
+        collegePlace: document.getElementById('collegePlaceInput').value
+    };
+    let usercollege = user.college;
+    console.log(usercollege);
+    usercollege.push(collegeIn);
+    user.college = usercollege;
+    setJSONLocalStorage(USER_INFO, user);
+    showProfile();
+}
+
+function addCity(){
+    let user = getJSONLocalStorage(USER_INFO);
+    let placeIn = {
+        placeName:document.getElementById('cityNameInput').value,
+        placeState:document.getElementById('cityStateInput').value
+    };
+
+    user.city.push(placeIn);
+    setJSONLocalStorage(USER_INFO, user);
+    showProfile();
+}
+
+function editDetails(){
+    let user = getJSONLocalStorage(USER_INFO);
+    user.about = document.getElementById('aboutInput').value;
+    user.otherName = document.getElementById('otherNameInput').value;
+    user.favQuote = document.getElementById('quoteInput').value;
+
+    setJSONLocalStorage(USER_INFO, user);
+    showProfile();
+}
+
+function editName(){
+    let user = getJSONLocalStorage(USER_INFO);
+    user.first_name = document.getElementById('fNameInput').value;
+    user.last_name = document.getElementById('lNameInput').value;
+
+    setJSONLocalStorage(USER_INFO, user);
+    // update current values
 }
