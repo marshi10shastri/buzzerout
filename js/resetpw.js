aac27edf4d28907b99d35951ba379ebc2bb33835
Pratyush Aswal <30869332+pratyush-aswal@users.noreply.github.com>
2020-04-28 16:45:33 +0530

    api added to forgotpwd

--- js/resetpw.js
+++ js/resetpw.js
@@ -3,18 +3,23 @@ function resetPassword(){
     if(emailInput && emailInput.value){
         $.ajax({
             type:'POST',
-            url:'',
+            url: SERVER_URL + 'user/forgotPassword',
             data:{
                 email: emailInput.value
             },
 
             success: function(data){
                 alert("Email has been sent");
+                console.log(data);
+                window.location = "sign-in.html";
             },
 
             error: function(data){
                 console.log(data);
             }
         });
+    }
+    else{
+        alert("enter email");
     }
 }
