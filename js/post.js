19ff642a0f9050ee7a2d28a01f528be0add18822
marshi <marshi10shastri@gmail.com>
2020-04-29 11:11:25 +0530

    corrected postjs

--- js/post.js
+++ js/post.js
@@ -9,7 +9,7 @@ function createPost() {
     var file = document.getElementById('buzz-photo-input').files[0];
     var resizedImage;
 
-    if(document.getElementById('buzz-photo-input').files.length == 0){
+    if (document.getElementById('buzz-photo-input').files.length == 0) {
         let user_name = getJSONLocalStorage(USER_INFO).username;
         let desc = document.getElementById('buzz-post-input').value;
         $.ajax({
@@ -25,7 +25,7 @@ function createPost() {
                 console.log(data);
                 var post = [{
                     feedid: data.feedid,
-                    username:getJSONLocalStorage(USER_INFO).username,
+                    username: getJSONLocalStorage(USER_INFO).username,
                     name: getJSONLocalStorage(USER_INFO).first_name,
                     userimage: getJSONLocalStorage(USER_INFO).userimage,
                     images: [],
@@ -43,8 +43,7 @@ function createPost() {
                 console.log(response)
             }
         });
-    }
-    else{
+    } else {
         // ---------------------------------------
         if (file.type.match(/image.*/)) {
             console.log('An image has been loaded');
@@ -112,55 +111,55 @@ function createPost() {
                 console.log(user_name);
                 console.log(desc);
                 // on success
-                    $.ajax({
-                        type: 'POST',
-                        url: 'http://buzzerout.com/buzzerout_server/v1/feed/uploadFeed',
-                        data: {
-                            username: user_name,
-                            title: 'title',
+                $.ajax({
+                    type: 'POST',
+                    url: 'http://buzzerout.com/buzzerout_server/v1/feed/uploadFeed',
+                    data: {
+                        username: user_name,
+                        title: 'title',
+                        description: desc,
+                        location: 'abc'
+                    },
+                    success: function(data) {
+                        console.log(data);
+                        let feedId = data.feedid;
+                        var post = [{
+                            feedid: data.feedid,
+                            username: getJSONLocalStorage(USER_INFO).username,
+                            name: getJSONLocalStorage(USER_INFO).first_name,
+                            userimage: getJSONLocalStorage(USER_INFO).userimage,
+                            images: [link],
                             description: desc,
-                            location: 'abc'
-                        },
-                        success: function(data) {
-                            console.log(data);
-                            let feedId = data.feedid;
-                            var post = [{
-                                feedid: data.feedid,
-                                username:getJSONLocalStorage(USER_INFO).username,
-                                name: getJSONLocalStorage(USER_INFO).first_name,
-                                userimage: getJSONLocalStorage(USER_INFO).userimage,
-                                images: [link],
-                                description: desc,
-                                timestamp: 'Just Now',
-                                likes: 0,
-                                comments: [],
-                            }];
-                            var local_posts = getJSONLocalStorage(POSTS);
-                            setJSONLocalStorage(POSTS, post.concat(local_posts));
-                            document.getElementById('close-modal').click();
-                            fetchPost();
-                            // fetchTimelinePosts();
+                            timestamp: 'Just Now',
+                            likes: 0,
+                            comments: [],
+                        }];
+                        var local_posts = getJSONLocalStorage(POSTS);
+                        setJSONLocalStorage(POSTS, post.concat(local_posts));
+                        document.getElementById('close-modal').click();
+                        fetchPost();
+                        // fetchTimelinePosts();
 
-                                //upload image to feed
-                                $.ajax({
-                                    type: 'POST',
-                                    url: 'http://buzzerout.com/buzzerout_server/v1/feed/uploadFeedImage',
-                                    data: {
-                                        feed_id:feedId,
-                                        img:link
-                                    },
-                                    success: function(data) {
-                                        console.log(data);
-                                    },
-                                    error: function(response) {
-                                        console.log(response);
-                                    }
-                                    });
-                        },
-                        error: function(data){
-                            console.log(data);
-                        }
-                    });
+                        //upload image to feed
+                        $.ajax({
+                            type: 'POST',
+                            url: 'http://buzzerout.com/buzzerout_server/v1/feed/uploadFeedImage',
+                            data: {
+                                feed_id: feedId,
+                                img: link
+                            },
+                            success: function(data) {
+                                console.log(data);
+                            },
+                            error: function(response) {
+                                console.log(response);
+                            }
+                        });
+                    },
+                    error: function(data) {
+                        console.log(data);
+                    }
+                });
             },
             error: function(error) {
                 console.log(error);
@@ -195,60 +194,7 @@ function fetchPost() {
     });
     // ---------------------------------
 
-    let data = getJSONLocalStorage(POSTS);
-    let inhtml = document.getElementById("posting-box").innerHTML;
-    inhtml = "";
-    inhtml += post_template_write_post(user.userimage);
-    for (let i = 0; i < data.length; i++) {
-        inhtml += post_template_userimage(data[i].userimage) +
-            post_template_username(data[i].username) +
-            post_template_time(data[i].timestamp) +
-            post_template_description(data[i].description, data[i].buzz_followed, data[i].feedid);
 
-        if (data[i].images.length > 0) {
-            if (data[i].images.length == 1) {
-                inhtml += post_template_image(data[i].images[0]);
-            } else if (data[i].images.length == 2) {
-                inhtml += post_template_image_two(data[i].images[0], data[i].images[1]);
-            } else if (data[i].images.length == 3) {
-                inhtml += post_template_image_three(data[i].images[0], data[i].images[1], data[i].images[2]);
-            } else {
-                inhtml += post_template_image_more(data[i].images[0], data[i].images[1], data[i].images[2]);
-            }
-        }
-
-        inhtml += post_template_likes(data[i].likes, data[i].buzz_upvoted, data[i].feedid) + post_template_comment_no(data[i].comments.length, data[i].buzz_shared, data[i].feedid);
-        if (data[i].comments.length > 0) {
-            for (let j = 0; j < data[i].comments.length; j++) {
-                inhtml += post_template_comment(data[i].comments[j].commentImg, data[i].comments[j].commentUser, data[i].comments[j].commentText);
-            }
-        }
-        inhtml += post_template_end(data[i].feedid)
-        feedInputArray.push("commentinput-" + data[i].feedid);
-        // console.log(inhtml);
-        // let inputCommentField = document.getElementById("commentinput-" + data[i].feedid);
-        // inputCommentField.addEventListener("keydown", function(e) {
-        //     if (e.keyCode === 13) {
-        //         console.log("hello")
-        //             //checks whether the pressed key is "Enter"
-        //         addComment(data[i].feedid, inputCommentField.value);
-        //     }
-        // })
-
-        // add event listener
-    }
-    document.getElementById("posting-box").innerHTML = inhtml;
-    for (let j = 0; j < feedInputArray.length; j++) {
-        let inputCommentField = document.getElementById(feedInputArray[j]);
-        inputCommentField.addEventListener("keydown", function(e) {
-            if (e.keyCode == 13) {
-                console.log('running');
-                let feedid = feedInputArray[j].split("-")[1];
-                addComment(feedid, inputCommentField.value);
-
-            }
-        })
-    }
 }
 
 function addComment(feedid, commentData) {
@@ -275,6 +221,90 @@ function addComment(feedid, commentData) {
                     //     timestamp: "Just Now"
                     // }
                     // temp.push(tempComment);
+
+
+                    let data = getJSONLocalStorage(POSTS);
+                    let inhtml = document.getElementById("posting-box").innerHTML;
+                    inhtml = "";
+                    inhtml += post_template_write_post(user.userimage);
+                    for (let i = 0; i < data.length; i++) {
+
+                        let timestring = "";
+                        let ts = data[i].timestamp.getTime();
+                        console.log(ts)
+                        let currts = Date.getTime()
+                        console.log(currts)
+                        diffts = (currts - ts) / 1000;
+                        if (diffts < 60) {
+                            timestring = "Less than a minute ago"
+                        } else {
+                            if (diffts / 60 < 60) {
+                                timestring = diffts / 60 + " mins ago"
+                            } else {
+                                difftsinmin = diffts / 60;
+                                if (difftsinmin < 60) {
+                                    timestring = difftsinmin + " mins ago"
+                                } else {
+
+                                    timestring = difftsinmin / 60 + " hours ago";
+                                }
+                            }
+                        }
+                        console.log(timestring)
+
+                        inhtml += post_template_userimage(data[i].userimage) +
+                            post_template_username(data[i].username) +
+                            post_template_time(timestring) +
+                            post_template_description(data[i].description, data[i].buzz_followed, data[i].feedid);
+
+                        if (data[i].images.length > 0) {
+                            if (data[i].images.length == 1) {
+                                inhtml += post_template_image(data[i].images[0]);
+                            } else if (data[i].images.length == 2) {
+                                inhtml += post_template_image_two(data[i].images[0], data[i].images[1]);
+                            } else if (data[i].images.length == 3) {
+                                inhtml += post_template_image_three(data[i].images[0], data[i].images[1], data[i].images[2]);
+                            } else {
+                                inhtml += post_template_image_more(data[i].images[0], data[i].images[1], data[i].images[2]);
+                            }
+                        }
+
+                        inhtml += post_template_likes(data[i].likes, data[i].buzz_upvoted, data[i].feedid) + post_template_comment_no(data[i].comments.length, data[i].buzz_shared, data[i].feedid);
+                        if (data[i].comments.length > 0) {
+                            for (let j = 0; j < data[i].comments.length; j++) {
+                                inhtml += post_template_comment(data[i].comments[j].commentImg, data[i].comments[j].commentUser, data[i].comments[j].commentText);
+                            }
+                        }
+                        inhtml += post_template_end(data[i].feedid)
+                        feedInputArray.push("commentinput-" + data[i].feedid);
+                        // console.log(inhtml);
+                        // let inputCommentField = document.getElementById("commentinput-" + data[i].feedid);
+                        // inputCommentField.addEventListener("keydown", function(e) {
+                        //     if (e.keyCode === 13) {
+                        //         console.log("hello")
+                        //             //checks whether the pressed key is "Enter"
+                        //         addComment(data[i].feedid, inputCommentField.value);
+                        //     }
+                        // })
+
+                        // add event listener
+                    }
+                    document.getElementById("posting-box").innerHTML = inhtml;
+                    for (let j = 0; j < feedInputArray.length; j++) {
+                        let inputCommentField = document.getElementById(feedInputArray[j]);
+                        inputCommentField.addEventListener("keydown", function(e) {
+                            if (e.keyCode == 13) {
+                                console.log('running');
+                                let feedid = feedInputArray[j].split("-")[1];
+                                addComment(feedid, inputCommentField.value);
+
+                            }
+                        })
+                    }
+
+
+
+
                     data[i].comments = data.comments;
                     setJSONLocalStorage(POSTS, data);
                     break;
@@ -314,13 +344,13 @@ function upvotePost(id) {
 
     //api call
     $.ajax({
-        type:'POST',
+        type: 'POST',
         url: SERVER_URL + 'feed/feedUpvote',
         data: {
             username: user.username,
             feed_id: postId
         },
-        success: function(data){
+        success: function(data) {
             console.log(data);
             for (let i = 0; i < posts.length; i++) {
                 if (posts[i].feedid == postId) {
@@ -333,7 +363,7 @@ function upvotePost(id) {
             // fetch all posts again
             fetchPost();
         },
-        error: function(data){
+        error: function(data) {
             console.log(data);
         }
     });
@@ -349,13 +379,13 @@ function downvotePost(id) {
 
     //api call
     $.ajax({
-        type:'POST',
+        type: 'POST',
         url: SERVER_URL + 'feed/feedDownvote',
         data: {
             username: user.username,
             feed_id: postId
         },
-        success: function(data){
+        success: function(data) {
             console.log(data);
             for (let i = 0; i < posts.length; i++) {
                 if (posts[i].feedid == postId) {
@@ -364,11 +394,11 @@ function downvotePost(id) {
                     break;
                 }
             }
-        
+
             // fetch all posts again
             fetchPost();
         },
-        error: function(data){
+        error: function(data) {
             console.log(data);
         }
     });
@@ -405,24 +435,24 @@ function followUser(id) {
 
     //api call
     $.ajax({
-        type:'POST',
+        type: 'POST',
         url: SERVER_URL + 'feed/feedUpvote',
         data: {
             followed_by: user.username,
             followes_to: posts[i].username
         },
-        success: function(data){
+        success: function(data) {
             console.log(data);
             posts[i].buzz_followed = true;
             setJSONLocalStorage(POSTS, posts);
 
             fetchPost();
         },
-        error: function(data){
+        error: function(data) {
             console.log(data);
         }
     });
 
 
-    
+
 }
