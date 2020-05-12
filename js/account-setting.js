ef2c45e0aa5ac96d8d6c02298c672422d0b09d60
Pratyush Aswal <30869332+pratyush-aswal@users.noreply.github.com>
2020-05-12 16:43:25 +0530

    edit done. account setting

--- js/account-setting.js
+++ js/account-setting.js
@@ -1,3 +1,12 @@
+function initAccount(){
+    renderAccountTopRight();
+    renderAccountTopMiddle();
+    renderAccountLeftBar();
+    setSocialInputValues();
+    hideSubmit();
+}
+
+function setSocialInputValues(){
 document.getElementById('uname').value = getUserDetails().uname;
 document.getElementById('email').value = getUserDetails().email;
 
@@ -6,7 +15,7 @@ document.getElementById('twitter').value = getUserSocialDetails().twitter;
 document.getElementById('google').value = getUserSocialDetails().google_plus;
 document.getElementById('instagram').value = getUserSocialDetails().instagram;
 document.getElementById('youtube').value = getUserSocialDetails().youtube;
-
+}
 
 function editSocialMedia() {
     console.log("hello")
@@ -31,10 +40,53 @@ function editSocialMedia() {
             document.getElementById('google').value = getUserSocialDetails().google_plus
             document.getElementById('instagram').value = getUserSocialDetails().instagram;
             document.getElementById('youtube').value = getUserSocialDetails().youtube;
+
+            //lock inputs
+            hideSubmit();
         },
         error: function(data) {
             console.log(data);
             setJSONLocalStorage("response", data)
         }
     });
+}
+
+
+function hideSubmit(){
+    document.getElementById('social-submit-btn').style.display = "none";
+    document.getElementById('social-cancel-btn').style.display = "none";
+    document.getElementById('social-edit-btn').style.display = "inline-block";
+
+    //disable inputs
+    disableSocialInputs();
+}
+
+function showSubmit(){
+    document.getElementById('social-edit-btn').style.display = "none";
+    document.getElementById('social-submit-btn').style.display = "inline-block";
+    document.getElementById('social-cancel-btn').style.display = "inline-block";
+
+    //enable inputs
+    enableSocialInputs();
+}
+
+function disableSocialInputs(){
+    document.getElementById('facebook').disabled = true;
+    document.getElementById('twitter').disabled = true;
+    document.getElementById('google').disabled = true;
+    document.getElementById('instagram').disabled = true;
+    document.getElementById('youtube').disabled = true;
+}
+
+function enableSocialInputs(){
+    document.getElementById('facebook').disabled = false;
+    document.getElementById('twitter').disabled = false;
+    document.getElementById('google').disabled = false;
+    document.getElementById('instagram').disabled = false;
+    document.getElementById('youtube').disabled = false;
+}
+
+function showEditBtn(){
+    setSocialInputValues();
+    hideSubmit();
 }
---
+++ js/header/accountSettingTopRight.js
@@ -0,0 +1,44 @@
+function renderAccountTopRight(){
+    if (getLocalStorage(USER) == 'true') {
+        document.getElementById('navbarSupportedContent').style.visibility = 'visible';
+        document.getElementById('person-name').textContent = getUserProfileDetails().fName;
+        document.getElementById('topnav-user-image').src = getUserProfileDetails().pImage;
+        document.getElementById('topnav-friend-icon').style.display = 'none';
+        document.getElementById('topnav-messages-dropdown').style.display = 'none';
+
+
+        document.getElementById('topnav-drop-profile').style.display = 'inline-block';
+        document.getElementById('topnav-drop-edit').style.display = 'inline-block';
+        document.getElementById('topnav-drop-account').style.display = 'inline-block';
+        document.getElementById('topnav-drop-privacy').style.display = 'inline-block';
+        document.getElementById('topnav-drop-username').textContent = getUserProfileDetails().fName;
+        document.getElementById('topnav-drop-status').textContent = getUserDetails().uname;
+        document.getElementById("logoutBtn").innerText = 'Sign Out';
+        console.log('user true');
+    } else {
+        // document.getElementById('navbarSupportedContent').style.visibility = 'hidden';
+        document.getElementById('person-name').style.display ='none';
+        document.getElementById('topnav-user-image').style.display = 'none';
+        document.getElementById('topnav-home').style.display = 'none';
+        document.getElementById('topnav-friend-icon').style.display = 'none';
+        document.getElementById('topnav-messages-dropdown').style.display = 'none';
+        document.getElementById('topnav-notification').style.display = 'none';
+
+        document.getElementById('topnav-drop-profile').style.display = 'none';
+        document.getElementById('topnav-drop-edit').style.display = 'none';
+        document.getElementById('topnav-drop-account').style.display = 'none';
+        document.getElementById('topnav-drop-privacy').style.display = 'none';
+        document.getElementById('topnav-drop-username').textContent = 'Anonymous';
+        document.getElementById('topnav-drop-status').textContent = 'Not Logged In';
+
+        document.getElementById("logoutBtn").innerText = 'Sign In';
+        console.log('user falser');
+    }
+
+    document.getElementById("logoutBtn").addEventListener('click', function(){
+        if(getLocalStorage(USER)){
+            logout();
+        }
+    });
+    
+}
