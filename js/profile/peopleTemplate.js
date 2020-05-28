d94fcda63be971442fc9424dfb5a638b88d690e3
Pratyush Aswal <30869332+pratyush-aswal@users.noreply.github.com>
2020-05-28 16:43:02 +0530

    follow following done in people

--- js/profile/peopleTemplate.js
+++ js/profile/peopleTemplate.js
@@ -1,45 +1,27 @@
 function singleFollower(person){
-   people = `<div class="col-md-6 col-lg-6 mb-3">
-
-    <div class="iq-friendlist-block">
-
-        <div class="d-flex align-items-center justify-content-between">
-
-            <div class="d-flex align-items-center">
-
-                <a href="#">
-
-                    <img src="`+person.image+`" alt="profile-img" class="img-fluid">
-
-                </a>
-
-                <div class="friend-info ml-3">
-
-                    <h5>`+ person.name +`</h5>
-
-                    <p class="mb-0">15 friends</p>
-
-                </div>
-
-            </div>
-
-            <div class="iq-card-header-toolbar d-flex align-items-center">
-
-                <div class="dropdown" id="button-`+ person.name +`">`;
-
-            let x = {
-                name:person.name,
-                image:person.image
-            }
+   people = '<div class="col-md-6 col-lg-6 mb-3">\
+    <div class="iq-friendlist-block">\
+        <div class="d-flex align-items-center justify-content-between">\
+            <div class="d-flex align-items-center">\
+                <a href="#">\
+                    <img src="'+person.image+'" alt="profile-img" class="img-fluid">\
+                </a>\
+                <div class="friend-info ml-3">\
+                    <h5>'+ person.name +'</h5>\
+                    <p class="mb-0">15 friends</p>\
+                </div>\
+            </div>\
+            <div class="iq-card-header-toolbar d-flex align-items-center">\
+                <div class="dropdown" id="button-"'+ person.name +'>';
             if(isPersonFollowed(person)){
                 console.log(person);
-                people +=    '<span class="dropdown-toggle btn btn-secondary mr-2" id="dropdownMenuButton01" aria-expanded="true" role="button" onclick="unfollowUserByUname(\''+ person.name+"-"+person.image  +'\')">\
-                            <i class="ri-check-line mr-1 text-white font-size-16"></i> Unfollow\
+                people +=    '<span class="dropdown-toggle btn btn-secondary mr-2" id="dropdownFollower-'+person.name+'" aria-expanded="true" role="button" onclick="followerUnfollowUserByUname(\''+ person.name+"-"+person.image  +'\')">\
+                            <i class="ri-close-line mr-1 text-white font-size-16"></i> Unfollow\
                             </span>';
             }
             else{
                 console.log(person);
-                people +=    '<span class="dropdown-toggle btn btn-secondary mr-2" id="dropdownMenuButton01" aria-expanded="true" role="button" onclick="followUserByUname(\''+ person.name+"-"+person.image  +'\')">\
+                people +=    '<span class="dropdown-toggle btn btn-primary mr-2" id="dropdownFollower-'+person.name+'" aria-expanded="true" role="button" onclick="followerFollowUserByUname(\''+ person.name+"-"+person.image  +'\')">\
                             <i class="ri-check-line mr-1 text-white font-size-16"></i> Follow\
                             </span>';
             }
@@ -58,41 +40,31 @@ return people;
 }
 
 function singleFollowing(person){
-    people = ` <div class="col-md-6 col-lg-6 mb-3">
-
-    <div class="iq-friendlist-block">
-
-        <div class="d-flex align-items-center justify-content-between">
-
-            <div class="d-flex align-items-center">
-
-                <a href="#">
-
-                    <img src="`+ person.image +`" alt="profile-img" class="img-fluid">
-
-                </a>
-
-                <div class="friend-info ml-3">
-
-                    <h5>`+person.name+`</h5>
-
-                    <p class="mb-0">4 friends</p>
-
-                </div>
-
-            </div>
-
-            <div class="iq-card-header-toolbar d-flex align-items-center">
-
-                <div class="dropdown">
-
-                    <span class="dropdown-toggle btn btn-secondary mr-2" id="dropdownMenuButton31" aria-expanded="true" role="button">
-
-   <i class="ri-check-line mr-1 text-white font-size-16"></i> Unfollow
-
-   </span>
-                </div>
-
+    people = ' <div class="col-md-6 col-lg-6 mb-3">\
+    <div class="iq-friendlist-block">\
+        <div class="d-flex align-items-center justify-content-between">\
+            <div class="d-flex align-items-center">\
+                <a href="#">\
+                    <img src="'+ person.image +'" alt="profile-img" class="img-fluid">\
+                </a>\
+                <div class="friend-info ml-3">\
+                    <h5>'+person.name+'</h5>\
+                    <p class="mb-0">4 friends</p>\
+              </div>\
+            </div>\
+            <div class="iq-card-header-toolbar d-flex align-items-center">\
+                <div class="dropdown" id="followingBtn-'+ person.name +'">';
+                if(isPersonFollowed(person)){
+                people += '<span class="dropdown-toggle btn btn-secondary mr-2" id="dropdownFollowing-'+person.name+'" aria-expanded="true" role="button" onclick="followingUnfollowUserByUname(\''+ person.name+"-"+person.image  +'\')">\
+                <i class="ri-close-line mr-1 text-white font-size-16"></i> Unfollow\
+                </span>';
+                }
+                else{
+                people += '<span class="dropdown-toggle btn btn-primary mr-2" id="dropdownFollowing-'+person.name+'" aria-expanded="true" role="button" onclick="followingFollowUserByUname(\''+ person.name+"-"+person.image  +'\')">\
+                <i class="ri-check-line mr-1 text-white font-size-16"></i> Follow\
+                </span>';
+                }
+            people +=    `</div>
             </div>
 
         </div>
@@ -105,40 +77,33 @@ return people;
 }
 
 function singleSuggestion(person){
-    people = `<div class="col-md-6 col-lg-6 mb-3">
+    people = '<div class="col-md-6 col-lg-6 mb-3">\
+    <div class="iq-friendlist-block">\
+        <div class="d-flex align-items-center justify-content-between">\
+            <div class="d-flex align-items-center">\
+                <a href="#">\
+                    <img src="'+person.image+'" alt="profile-img" class="img-fluid">\
+                </a>\
+                <div class="friend-info ml-3">\
+                    <h5>'+person.name+'</h5>\
+                    <p class="mb-0">32 friends</p>\
+                </div>\
+            </div>\
+            <div class="iq-card-header-toolbar d-flex align-items-center">\
+                <div class="dropdown" id="buttonSuggested-'+person.name+'">';
 
-    <div class="iq-friendlist-block">
 
-        <div class="d-flex align-items-center justify-content-between">
-
-            <div class="d-flex align-items-center">
-
-                <a href="#">
-
-                    <img src="`+person.image+`" alt="profile-img" class="img-fluid">
-
-                </a>
-
-                <div class="friend-info ml-3">
-
-                    <h5>`+person.name+`</h5>
-
-                    <p class="mb-0">32 friends</p>
-
-                </div>
-
-            </div>
-
-            <div class="iq-card-header-toolbar d-flex align-items-center">
-
-                <div class="dropdown">
-
-                    <span class="dropdown-toggle btn btn-secondary mr-2" id="dropdownMenuButton39" aria-expanded="true" role="button">
-
-   <i class="ri-check-line mr-1 text-white font-size-16"></i> Follow
-
-   </span>
-                </div>
+                if(isPersonFollowed(person)){
+                    people += '<span class="dropdown-toggle btn btn-secondary mr-2" id="dropdownSuggested-'+person.name+'" aria-expanded="true" role="button" onclick="suggestedUnfollowUserByUname(\''+ person.name+"-"+person.image  +'\')">\
+                    <i class="ri-close-line mr-1 text-white font-size-16"></i> Unfollow\
+                    </span>';
+                    }
+                    else{
+                    people += '<span class="dropdown-toggle btn btn-primary mr-2" id="dropdownSuggested-'+person.name+'" aria-expanded="true" role="button" onclick="suggestedFollowUserByUname(\''+ person.name+"-"+person.image  +'\')">\
+                    <i class="ri-check-line mr-1 text-white font-size-16"></i> Follow\
+                    </span>';
+                    }
+                people += `</div>
 
             </div>
 
@@ -147,7 +112,6 @@ function singleSuggestion(person){
     </div>
 
 </div>`;
-
 return people;
 }
 
@@ -165,7 +129,9 @@ function isPersonFollowed(person){
     return false;
 }
 
-function unfollowUserByUname(pers){
+
+//follower section
+function followerUnfollowUserByUname(pers){
     console.log(pers);
     let pname = pers.split("-")[0];
     let pImage = pers.split("-")[1];
@@ -188,8 +154,9 @@ function unfollowUserByUname(pers){
                     //update local
                     updateUserFollowing(data.following);
 
+                    //followers
                     let div = document.getElementById("button-"+ person.name);
-                    div.innerHTML = '<span class="dropdown-toggle btn btn-secondary mr-2" id="dropdownMenuButton01" aria-expanded="true" role="button" onclick="followUserByUname(\''+ person.name+"-"+person.image  +'\')">\
+                    div.innerHTML = '<span class="dropdown-toggle btn btn-primary mr-2" id="dropdownFollower-'+person.name+'" aria-expanded="true" role="button" onclick="followerFollowUserByUname(\''+ person.name+"-"+person.image  +'\')">\
                     <i class="ri-check-line mr-1 text-white font-size-16"></i> Follow\
                     </span>'
                 }
@@ -206,7 +173,51 @@ function unfollowUserByUname(pers){
 }
 
 
-function followUserByUname(pers){
+//following section
+function followingUnfollowUserByUname(pers){
+    console.log(pers);
+    let pname = pers.split("-")[0];
+    let pImage = pers.split("-")[1];
+    let person = {
+        name:pname,
+        image:pImage
+    }
+    $.ajax({
+            type:'POST',
+            url:SERVER_URL + 'follow/deleteFollowing',
+            data:{
+                username: getUserDetails().uname,
+                user_to_deleted: person.name
+            },
+
+            success: function(data){
+                if(data.error == false){
+                    //on success
+                    console.log(data);
+                    //update local
+                    updateUserFollowing(data.following);
+
+                    //followers
+                    let div = document.getElementById("followingBtn-"+ person.name);
+                    div.innerHTML = '<span class="dropdown-toggle btn btn-primary mr-2" id="dropdownFollowing-'+person.name+'" aria-expanded="true" role="button" onclick="followingFollowUserByUname(\''+ person.name+"-"+person.image  +'\')">\
+                    <i class="ri-check-line mr-1 text-white font-size-16"></i> Follow\
+                    </span>'
+                }
+                else{
+                    console.log(data);
+                    console.log('error following');
+                }
+            },
+            error: function(data){
+                console.log('follow api error');
+                console.log(data);
+            }
+        });
+}
+
+
+//follower section
+function followerFollowUserByUname(pers){
     console.log(pers);
     let pname = pers.split("-")[0];
     let pImage = pers.split("-")[1];
@@ -231,8 +242,136 @@ function followUserByUname(pers){
                 updateUserFollowing(data.following);
 
                 let div = document.getElementById("button-"+ person.name);
-                div.innerHTML = '<span class="dropdown-toggle btn btn-secondary mr-2" id="dropdownMenuButton01" aria-expanded="true" role="button" onclick="unfollowUserByUname(\''+ person.name+"-"+person.image  +'\')">\
-                <i class="ri-check-line mr-1 text-white font-size-16"></i> Unfollow\
+                div.innerHTML = '<span class="dropdown-toggle btn btn-secondary mr-2" id="dropdownFollower-'+person.name+'" aria-expanded="true" role="button" onclick="followerUnfollowUserByUname(\''+ person.name+"-"+person.image  +'\')">\
+                <i class="ri-close-line mr-1 text-white font-size-16"></i> Unfollow\
+                </span>'
+            }
+            else{
+                console.log('error following');
+                console.log(data)
+            }
+        },
+        error: function(data){
+            console.log('follow api error');
+            console.log(data);
+        }
+    });
+}
+
+
+//following section
+function followingFollowUserByUname(pers){
+    console.log(pers);
+    let pname = pers.split("-")[0];
+    let pImage = pers.split("-")[1];
+    let person = {
+        name:pname,
+        image:pImage
+    }
+    $.ajax({
+        type:'POST',
+        url:SERVER_URL + 'follow/newFollow',
+        data:{
+            followed_by: getUserDetails().uname,
+            followes_to: person.name
+        },
+
+        success: function(data){
+            console.log(data);
+            if(data.error == false){
+                //on success
+                console.log(data);
+                
+                updateUserFollowing(data.following);
+
+                let div = document.getElementById("followingBtn-"+ person.name);
+                div.innerHTML = '<span class="dropdown-toggle btn btn-secondary mr-2" id="dropdownFollowing-'+person.name+'" aria-expanded="true" role="button" onclick="followingUnfollowUserByUname(\''+ person.name+"-"+person.image  +'\')">\
+                <i class="ri-close-line mr-1 text-white font-size-16"></i> Unfollow\
+                </span>'
+            }
+            else{
+                console.log('error following');
+                console.log(data)
+            }
+        },
+        error: function(data){
+            console.log('follow api error');
+            console.log(data);
+        }
+    });
+}
+
+
+//suggestions section
+function suggestedUnfollowUserByUname(pers){
+    console.log(pers);
+    let pname = pers.split("-")[0];
+    let pImage = pers.split("-")[1];
+    let person = {
+        name:pname,
+        image:pImage
+    }
+    $.ajax({
+            type:'POST',
+            url:SERVER_URL + 'follow/deleteFollowing',
+            data:{
+                username: getUserDetails().uname,
+                user_to_deleted: person.name
+            },
+
+            success: function(data){
+                if(data.error == false){
+                    //on success
+                    console.log(data);
+                    //update local
+                    updateUserFollowing(data.following);
+
+                    //followers
+                    let div = document.getElementById("buttonSuggested-"+ person.name);
+                    div.innerHTML = '<span class="dropdown-toggle btn btn-primary mr-2" id="dropdownSuggested-'+person.name+'" aria-expanded="true" role="button" onclick="suggestedFollowUserByUname(\''+ person.name+"-"+person.image  +'\')">\
+                    <i class="ri-check-line mr-1 text-white font-size-16"></i> Follow\
+                    </span>'
+                }
+                else{
+                    console.log(data);
+                    console.log('error following');
+                }
+            },
+            error: function(data){
+                console.log('follow api error');
+                console.log(data);
+            }
+        });
+}
+
+
+function suggestedFollowUserByUname(pers){
+    console.log(pers);
+    let pname = pers.split("-")[0];
+    let pImage = pers.split("-")[1];
+    let person = {
+        name:pname,
+        image:pImage
+    }
+    $.ajax({
+        type:'POST',
+        url:SERVER_URL + 'follow/newFollow',
+        data:{
+            followed_by: getUserDetails().uname,
+            followes_to: person.name
+        },
+
+        success: function(data){
+            console.log(data);
+            if(data.error == false){
+                //on success
+                console.log(data);
+                
+                updateUserFollowing(data.following);
+
+                let div = document.getElementById("buttonSuggested-"+ person.name);
+                div.innerHTML = '<span class="dropdown-toggle btn btn-secondary mr-2" id="dropdownSuggested-'+person.name+'" aria-expanded="true" role="button" onclick="suggestedUnfollowUserByUname(\''+ person.name+"-"+person.image  +'\')">\
+                <i class="ri-close-line mr-1 text-white font-size-16"></i> Unfollow\
                 </span>'
             }
             else{
