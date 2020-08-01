1f4f642b07b33a1eb5fb25116e0d4313e964315d
Pratyush Aswal <30869332+pratyush-aswal@users.noreply.github.com>
2020-08-01 11:20:27 +0530

    logoutuser

--- js/buzzerout/index/latest-news.js
+++ js/buzzerout/index/latest-news.js
@@ -13,7 +13,27 @@ function renderLatestNews(){
 
     }
     else if(getLocalStorage(USER_TYPE) == 'logoutuser'){
+        $.ajax({
+            type: 'POST',
+            url: SERVER_URL + 'updates/fetchAllUpdates',
+            success: function(data){
+                console.log(data);
+                if(data.error == false){
+                    let news = data.updates;
 
+                    if(news.length > 0){
+                        for(let i=0; i<news.length; i++){
+                            newsList.innerHTML += singleLatestNews(news[i]);
+                        }
+                    }
+                }else{
+                    console.log(data.message);
+                }
+            },
+            error: function(data){
+                console.log(data);
+            }
+        });
     }
     else if(getLocalStorage(USER_TYPE) == 'liveuser'){
         //ajax
@@ -42,21 +62,21 @@ function renderLatestNews(){
 function singleLatestNews(news){
     post = '<li class="mb-3">\
             <div class="d-flex align-items-center mb-3">\
-                <img src="'+ news.smallimg +'" class="rounded-circle img-fluid avatar-50">\
+                <img src="'+ news.small_image +'" class="rounded-circle img-fluid avatar-50">\
                 <div class="stories-data ml-3">\
                     <h5>'+ news.title +'</h5>\
                     <p class="mb-0">'+ news.subtitle +'</p>\
                 </div>\
             </div>\
-            <img src="'+ news.bigimg +'" class="img-fluid rounded" alt="Responsive image">\
-            <div class="mt-3"><a href="'+ news.href +'" class="btn d-block"><i class="ri-links-line"></i> Visit Link </a></div>\
+            <img src="'+ news.large_image +'" class="img-fluid rounded" alt="Responsive image">\
+            <div class="mt-3"><a href="'+ news.link +'" class="btn d-block"><i class="ri-links-line"></i> Visit Link </a></div>\
             </li>'
 
     return post;
 }
 
 const LATEST_NEWS = [
-    {title:'Iqonic Studio', subtitle:'Lorem Ipsum', smallimg:'images/page-img/42.png', bigimg:'images/small/img-1.jpg', href:'http://www.google.com'},
-    {title:'Eyeconic Xtudeo', subtitle:'Different Spelling', smallimg:'images/page-img/43.png', bigimg:'images/small/img-2.jpg', href:'http://www.google.com'},
-    {title:'Ayeecownic Estudio', subtitle:'Latest News', smallimg:'images/page-img/46.png', bigimg:'images/small/img-1.jpg', href:'http://www.google.com'}
+    {title:'Iqonic Studio', subtitle:'Lorem Ipsum', small_image:'images/page-img/42.png', large_image:'images/small/img-1.jpg', link:'http://www.google.com'},
+    {title:'Eyeconic Xtudeo', subtitle:'Different Spelling', small_image:'images/page-img/43.png', large_image:'images/small/img-2.jpg', link:'http://www.google.com'},
+    {title:'Ayeecownic Estudio', subtitle:'Latest News', small_image:'images/page-img/46.png', large_image:'images/small/img-1.jpg', link:'http://www.google.com'}
 ]
